7b8e57b6800f2f36ce79515dff01ea2d32dd51da
Awesome <awesome_devnet@outlook.com>
2024-06-19 17:49:37 +0900

    프로젝트 베이스 초기화

---
+++ src/main/java/com/ohgiraffers/developershop/DeveloperShopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.developershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperShopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperShopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/developershop/DeveloperShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.developershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperShopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
