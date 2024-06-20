874f5da1ecff6abe0968bcfb85f0464736cf2654
Awesome <awesome_devnet@outlook.com>
2024-06-20 14:54:27 +0900

    프로젝트 베이스 수정

--- src/main/java/com/nf404/devshop/DeveloperShopProjectApplication.java
+++ src/main/java/com/nf404/devshop/DeveloperShopProjectApplication.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.developershop;
+package com.nf404.devshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nf404/devshop/DeveloperShopProjectApplicationTests.java
+++ src/test/java/com/nf404/devshop/DeveloperShopProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.developershop;
+package com.nf404.devshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
